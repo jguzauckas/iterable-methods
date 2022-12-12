72da068db03a95d4cc8cf31a5e76951ed0f18e6d
John Guzauckas <75325154+jguzauckas@users.noreply.github.com>
2022-12-12 13:15:00 -0500

    Added files

---
+++ classwork.py
@@ -0,0 +1,22 @@
+# Create a list with 20 random numbers with values between 0 and 10.
+# Print out the list once it is completed.
+
+
+# Print out the sum, maximum value, and minimum value of our list with
+# our new methods!
+
+
+# Print out the number of times that the number `5` shows up in the list.
+
+
+# Sort the list in ascending order and print the result.
+
+
+# Now sort the list in descending order and print the result.
+
+
+# Check if the number 0 appears in the list and print the result as a boolean.
+
+
+# Print the indices (index) at which the number 8 appears in the list.
+# Make sure it prints all the indices it shows up at, and not just the first!
